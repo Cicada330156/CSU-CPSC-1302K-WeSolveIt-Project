4a9d1e7a9172a04a4f5c75090cf07db2da1247bc
animeloverofjustice <150277765+animeloverofjustice@users.noreply.github.com>
2023-11-14 21:02:58 -0500

    Update GrillRecipe.java

---
+++ GrillRecipe.java
@@ -0,0 +1,9 @@
+/**
+ * In this class we will extend recipe, use instance variables, boolean, a grill temp as well.
+ * Then we will use methods get or set variables and the override these methods. 
+ * @author Ryan McKelphin
+ *@version 1.0
+ */
+public class GrillRecipe {
+
+}
