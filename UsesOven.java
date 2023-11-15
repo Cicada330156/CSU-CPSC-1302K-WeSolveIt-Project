7755c86f488aba696d2cfa610863f2eb4a80c173
animeloverofjustice <150277765+animeloverofjustice@users.noreply.github.com>
2023-11-14 20:58:04 -0500

    Update UsesOven.java

---
+++ UsesOven.java
@@ -0,0 +1,10 @@
+/**
+ * We will use this class/ interface for instance variables, a boolean preheat.
+ * We will also use methods that include get or set methods for the preheat.
+ * @author Ryan McKelphin
+ * @version 1.0
+ *
+ */
+public class UsesOven {
+
+}
