46944cf91a45384671cccc9512b2343002a03ca3
animeloverofjustice <150277765+animeloverofjustice@users.noreply.github.com>
2023-11-14 21:05:07 -0500

    Update BakingRecipe.java

---
+++ BakingRecipe.java
@@ -0,0 +1,11 @@
+/**
+ * for the BakingRecipe we will extend recipe implements uses oven
+ * I will also use boolean preheat, oven temp, methods that are not overridden that is get set instance variables
+ * THen I will override these with format as JSON, toStrig, equals, and editRecipe
+ * @author Ryan McKelphin
+ * @version 1.0
+ *
+ */
+public class BakingRecipe {
+
+}
