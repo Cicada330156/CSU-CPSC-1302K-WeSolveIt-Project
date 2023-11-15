e2af074d66314bba5c3a3eb2a708dac0b6cfb86c
animeloverofjustice <150277765+animeloverofjustice@users.noreply.github.com>
2023-11-14 21:00:13 -0500

    Update StovetopRecipe.java

---
+++ StovetopRecipe.java
@@ -0,0 +1,8 @@
+/**
+ * This class will uses extend, temp Strings, pot types, methods that notoveridden, and overridden methods as well.
+ *@author Ryan McKelphin
+ *@version 1.0
+ */
+public class StovetopRecipe {
+
+}
