49982ab4e1aa9068616d9aadf1f5f405965c17d9
1rishj0hn <hendricks.sean.a@gmail.com>
2023-11-21 00:01:44 -0500

    current progress on RecipeBook driver class.

---
+++ RecipeBook.java
@@ -0,0 +1,27 @@
+/**
+ * Driver code that implements the Recipe class.
+ *
+ * @author Sean Hendricks, Ryan Mckelphin, Ari-Betan Snook
+ * @version 1.0
+ */
+import java.util.Scanner;
+public class RecipeBook{
+
+	public static void main( String[] args ){
+		System.out.println( "Welcome to your recipe manager!\n" );
+		
+		Scanner echo = new Scanner( System.in );
+		String select = "";
+		/**
+		 * Asks the user if they want to edit a recipe.
+		 */
+
+		/**
+		 * Asks the user if they want to display all recipes.
+		 */
+
+		/**
+		 * Asks the user if they want to display a recipe.
+		 */
+	}
+}
